c0a6d94e8ae63b92034ce5382773de8dfea3bd7b
Vladimir Rumyancev <53127742+Shakjamuni@users.noreply.github.com>
2020-10-31 14:50:29 +0300

    https://github.com/kotsnataly/java_webtesters_less8_hw

--- src/main/java/controller/Vault.java
+++ src/main/java/controller/Vault.java
@@ -5,9 +5,7 @@ import com.fasterxml.jackson.databind.ObjectWriter;
 import model.Transaction;
 import model.User;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,40 +20,37 @@ public class Vault implements Xeon {
 
     //логика отправки пользователем денег, с проверками на сумму баланса пользователя и сумму самого хранилища.
     public boolean makeTransaction(User userFrom, User userTo, float amount) throws IOException {
+
+        //маппер на будущее для сериализации.
+        //Пыталась использовать System.out для вывода в json формате, но программа закрывается при первой транзакции, не могу пока исправить.
         ObjectMapper mapper = new ObjectMapper();
         ObjectWriter objectWriter = mapper.writerFor(Transaction.class);
+
         Transaction t = new Transaction();
         if (amount > this.totalVaultAmount) {
             //total vault sum is less than amount
-            {
-                t.setSuccess(false);
-                t.setReason("Amount is too large for the vault");
-                t.setAmount(amount);
-                transactionList.add(t);
-            }
-            return t.isSuccess();
+            t.setSuccess(false);
+            t.setReason("Amount is too large for the vault");
+            t.setAmount(amount);
+            transactionList.add(t);
 
         } else if (userFrom.getUserAccount().getTotalSum() < amount) {
             //total sum is less than transaction sum
-            {
-                t.setSuccess(false);
-                t.setReason("This amount is inaccessible for the user");
-                t.setAmount(amount);
-                transactionList.add(t);
-            }
-            return t.isSuccess();
+            t.setSuccess(false);
+            t.setReason("This amount is inaccessible for the user");
+            t.setAmount(amount);
+            transactionList.add(t);
         } else {
-            {
-                userFrom.getUserAccount().setTotalSum(-amount);
-                userTo.getUserAccount().setTotalSum(amount);
+            userFrom.getUserAccount().setTotalSum(-amount);
+            userTo.getUserAccount().setTotalSum(amount);
 
-                t.setSuccess(true);
-                t.setReason("Transaction done");
-                t.setAmount(amount);
-                transactionList.add(t);
-            }
-            return t.isSuccess();
+            t.setSuccess(true);
+            t.setReason("Transaction done");
+            t.setAmount(amount);
+            transactionList.add(t);
         }
+        System.out.println(t);
+        return t.isSuccess();
     }
 
     public List<Transaction> getTransactionList() {
